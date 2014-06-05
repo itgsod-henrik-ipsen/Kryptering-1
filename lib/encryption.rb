9049d96fa4854d5961bbfe0176fcbd914b6abad1
itgsod-henrik-ipsen <henrik.ipsen@elev.it-gymnasiet.se>
2014-06-05 09:25:39 +0200

    return error if input is nothing

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,4 +1,6 @@
 def encrypt(input, offset)
+    raise ArgumentError, 'String must not be empty' if input == ''
+    
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
     input = input.upcase
