7b2d316bf16df4d5c6aa6775b2ffec2468052904
itgsod-henrik-ipsen <henrik.ipsen@elev.it-gymnasiet.se>
2014-06-05 09:22:36 +0200

    it now takes a string and offset as arguments

---
+++ lib/encryption.rb
@@ -0,0 +1,12 @@
+def encrypt(input, offset)
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    input = input.upcase
+    inputList = ([] << input)
+
+    output = ''
+
+    return output
+
+end
+
