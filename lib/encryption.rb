b3a083edb4f7c3f3120a7e4ccfa5c4d1b8001e61
itgsod-henrik-ipsen <henrik.ipsen@elev.it-gymnasiet.se>
2014-06-05 09:29:37 +0200

    returns error if offset == 0

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,6 +1,6 @@
 def encrypt(input, offset)
     raise ArgumentError, 'String must not be empty' if input == ''
-    
+    raise ArgumentError, 'Offset must not be zero' if offset == 0
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
     input = input.upcase
